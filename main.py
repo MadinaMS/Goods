d4eb9ac3bee6aed780f4c77639481d8a542e8dee
MadinaMS <mserikovna@gmail.com>
2023-03-12 01:01:52 +0600

    adds class Cart

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-class Good:
+class Good():
     def __init__(self, name, price, quantity=1):
         self.name = name
         self.price = price
@@ -16,20 +16,51 @@ class Good:
     def __str__(self):
         return f"{self.name:<20} {self.price:>7.2f}*{self.quantity:>3} = {self.get_sum():<5.2f}"
 
-class DiscountedGood:
-    def __init__(self,  name, price, quantity=1, discount = 0):
-        super().__init__(name, price, quantity=1)
+class DiscountedGood(Good):
+    def __init__(self,  name, price, quantity=1, discount=0):
+        super().__init__(name, price, quantity)
         self.discount = discount
 
     def get_sum(self):
-        return super().get_sum()((100-self.discount)/100)
+        return super().get_sum() * ((100-self.discount)/100)
 
     def __str__(self):
         return super().__str__() + f"(-{self.discount}%)"
 
-good = Good("Bread", 17, 3)
-print(good)
-discounted_good = DiscountedGood("Milk", 500, 1, 10)
-print(discounted_good)
+class Cart():
+    def __init__(self, goods_list):
+        self.goods_list = goods_list
+
+    def total_sum(self):
+        total_sum = 0
+        for good in self.goods_list:
+            if isinstance(good, DiscountedGood) and good.discount > 0:
+                total_sum += good.price * good.quantity - good.price/100 * good.discount
+            else:
+                total_sum += good.price * good.quantity
+        return total_sum
+
+    def display_cart(self):
+        print("{:<20} {:>7} {:>5} {:>10} {}:6".format("Name", "PPU", "CNT", "Price", "Disc."))
+        print("===================================================")
+        for good in self.goods_list:
+            print(good)
+        print("===================================================")
+        print("{:<30} = {:<10.2f}".format("Total", self.total_sum()))
+
+goods = [
+    Good('Bread', 17, 3),
+    Good('Water', 19, 2),
+    DiscountedGood("Juice", 80, 1, 20)
+]
+
+cart = Cart(goods)
+cart.display_cart()
+
+
+#good = Good("Bread", 17, 3)
+#print(good)
+#discounted_good = DiscountedGood("Milk", 500, 1, 10)
+#print(discounted_good)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
